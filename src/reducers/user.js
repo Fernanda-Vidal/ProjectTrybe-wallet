73aefe8fd102db31f401dab1e34e56652b6f2359
Fernanda Vidal <fevidal.dev@gmail.com>
2022-06-22 16:35:48 -0300

    Cria reducers (default) e combineReducers.

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,15 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+const INITIAL_STATE = {
+  user: {
+    email: '', // string que armazena o email da pessoa usuária
+  },
+};
+
+const userReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  default:
+    return state;
+  }
+};
+
+export default userReducer;
