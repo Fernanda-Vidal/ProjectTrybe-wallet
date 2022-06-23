c444c52926672777d30ee97b780e318a0e8ee086
Fernanda Vidal <fevidal.dev@gmail.com>
2022-06-23 16:30:56 -0300

    Implementa mapStateToProps.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,18 @@
+import PropTypes from 'prop-types';
 import React from 'react';
+import { connect } from 'react-redux';
 
 class Header extends React.Component {
   render() {
+    const { userState } = this.props;
     return (
       <div>
         <header>
           <h1>TrybeWallet</h1>
-          <span data-testid="email-field">Email:</span>
+          <span data-testid="email-field">
+            Email:
+            {userState}
+          </span>
           <span data-testid="total-field">Despesa Total: 0</span>
           <span data-testid="header-currency-field">BRL</span>
         </header>
@@ -15,4 +21,12 @@ class Header extends React.Component {
   }
 }
 
-export default Header;
+Header.propTypes = {
+  userState: PropTypes.string.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  userState: state.user.email,
+});
+
+export default connect(mapStateToProps)(Header);
