13577c67ce8ec8e617c478d4e3038f40dd3e3e0f
Fernanda Vidal <fevidal.dev@gmail.com>
2022-06-24 16:01:29 -0300

    Requisito 5 finalizado.

--- src/components/Forms.js
+++ src/components/Forms.js
@@ -1,26 +1,53 @@
+// import { element } from 'prop-types';
+import PropTypes from 'prop-types';
 import React from 'react';
-import InputSelect from './Select';
+import { connect } from 'react-redux';
 import '../css/wallet.css';
 
 class Forms extends React.Component {
+  generateSelect = (array) => (
+    array.map((item, i) => (
+      <option name={ item } key={ `${item}-${i}` }>{ item }</option>
+    ))
+  );
+
   render() {
+    const paymentMethod = ['Dinheiro', 'Cartão de crédito', 'Cartão de débito'];
+    const { walletState } = this.props;
+    const arrayTag = ['Alimentação', 'Lazer', 'Trabalho', 'Transporte', 'Saúde'];
     return (
       <div>
         <div>
           <form className="forms-wallet">
             <label htmlFor="valor">
               Valor:
-              <input data-testid="value-input" type="number" />
+              <input type="number" data-testid="value-input" />
             </label>
-            <InputSelect
-              data-testid="method-input"
-              nome="Metodo"
-              label="Metodo de pagamento"
-            />
-            <InputSelect nome="Moeda" label="Moeda" />
-            <InputSelect data-testid="tag-input" nome="Tag" label="Tag" />
+            <label htmlFor="moeda">
+              Moeda
+              <select id="moeda">
+                {this.generateSelect(walletState)}
+              </select>
+            </label>
+            <label htmlFor="metodo">
+              Método de pagamento
+              <select
+                data-testid="method-input"
+              >
+                {this.generateSelect(paymentMethod)}
+              </select>
+            </label>
+            <label htmlFor="tag">
+              Tag
+              <select
+                data-testid="tag-input"
+              >
+                {this.generateSelect(arrayTag)}
+              </select>
+            </label>
             <label htmlFor="descricao">
-              <input data-testid="description-input" type="text" />
+              Descrição
+              <input type="text" data-testid="description-input" />
             </label>
             <button type="button">Adicionar despesa</button>
           </form>
@@ -30,4 +57,12 @@ class Forms extends React.Component {
   }
 }
 
-export default Forms;
+Forms.propTypes = {
+  walletState: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  walletState: state.wallet.currencies,
+});
+
+export default connect(mapStateToProps)(Forms);
--- src/components/Select.js
+++ src/components/Select.js
@@ -1,25 +1,25 @@
-import PropTypes from 'prop-types';
-import React from 'react';
+// import PropTypes from 'prop-types';
+// import React from 'react';
 
-class InputSelect extends React.Component {
-  render() {
-    const { nome, label } = this.props;
-    return (
-      <div>
-        <label htmlFor={ label }>
-          { label }
-          <select>
-            <options name={ nome }>{ nome }</options>
-          </select>
-        </label>
-      </div>
-    );
-  }
-}
+// class InputSelect extends React.Component {
+//   render() {
+//     const { nome, label } = this.props;
+//     return (
+//       <div>
+//         <label htmlFor={ label }>
+//           { label }
+//           <select>
+//             <options name={ nome }>{ nome }</options>
+//           </select>
+//         </label>
+//       </div>
+//     );
+//   }
+// }
 
-InputSelect.propTypes = {
-  label: PropTypes.string.isRequired,
-  nome: PropTypes.string.isRequired,
-};
+// InputSelect.propTypes = {
+//   label: PropTypes.string.isRequired,
+//   nome: PropTypes.string.isRequired,
+// };
 
-export default InputSelect;
+// export default InputSelect;
